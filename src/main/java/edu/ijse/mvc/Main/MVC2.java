92c69fbd2e5483dc6d2a6dbdfdfb88952a7c21e4
Lahiru075 <sanjeewalahiru057@gmail.com>
2024-09-11 09:53:41 +0530

    first commit

---
+++ src/main/java/edu/ijse/mvc/Main/MVC2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.ijse.mvc.Main;
+
+/**
+ *
+ * @author lahiru
+ */
+public class MVC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
